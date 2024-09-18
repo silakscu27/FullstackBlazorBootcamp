2a724826023a314a52f962ec1f6a48c834f8975b
Sila Kuscu <silaksc27@gmail.com>
2024-09-18 15:55:46 +0300

    Video Transcriber app SSR transactions have been completed

---
+++ FullstackBlazorBootcamp/AI.VideoTranscriberApp.BlazorUI/AI.VideoTranscriberApp.BlazorUI.Client/Models/TranscriptionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AI.VideoTranscriberApp.BlazorUI.Client.Models
+{
+    public class TranscriptionModel
+    {
+        public string Language { get; set; }
+        public string Text { get; set; }
+    }
+}
