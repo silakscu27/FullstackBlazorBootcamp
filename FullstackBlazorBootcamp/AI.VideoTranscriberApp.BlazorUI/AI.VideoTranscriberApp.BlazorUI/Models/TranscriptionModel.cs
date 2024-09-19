35607370b899c45b4c8ccdabbf0a3272d56ff8da
Sila Kuscu <silaksc27@gmail.com>
2024-09-19 17:08:22 +0300

    Create TranscriptionModel.cs

---
+++ FullstackBlazorBootcamp/AI.VideoTranscriberApp.BlazorUI/AI.VideoTranscriberApp.BlazorUI/Models/TranscriptionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AI.VideoTranscriberApp.BlazorUI.Models
+{
+    public class TranscriptionModel
+    {
+        public string Language { get; set; }
+        public string Text { get; set; }
+    }
+}
